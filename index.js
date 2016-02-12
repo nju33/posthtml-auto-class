6e481c15279706986640c24415c89c64fbda7c6b
nju33 <nju33@nmba.local>
2016-02-13 04:31:47 +0900

    Change default value of `aliasNames`

--- index.js
+++ index.js
@@ -4,29 +4,7 @@ const _ = require('lodash');
 const Node = require('./lib/node');
 
 let scopeNames = [];
-let aliasNames = {
-  a: 'link',
-  img: 'img',
-  hr: 'hr',
-  ul: 'list',
-  li: 'item',
-  dl: 'list',
-  dt: 'term',
-  dd: 'desc',
-  code: 'code',
-  pre: 'pre',
-  blockquote: 'blockquote',
-  form: 'form',
-  label: 'label',
-  input: 'input',
-  select: 'select',
-  button: 'btn',
-  main: 'main',
-  archive: 'archive',
-  header: 'header',
-  footer: 'footer',
-  progress: 'progress',
-};
+let aliasNames = {};
 
 module.exports = posthtmlAutoClass;
 
--- test/build.js
+++ test/build.js
@@ -4,7 +4,30 @@ const beautify = require('js-beautify').html;
 const html = require('fs').readFileSync('./test/index.html', 'utf-8');
 
 posthtml([autoClass({
-  scopeNames: ['box', 'block']
+  scopeNames: ['box', 'block'],
+  aliasNames: {
+    a: 'link',
+    img: 'img',
+    hr: 'hr',
+    ul: 'list',
+    li: 'item',
+    dl: 'list',
+    dt: 'term',
+    dd: 'desc',
+    code: 'code',
+    pre: 'pre',
+    blockquote: 'blockquote',
+    form: 'form',
+    label: 'label',
+    input: 'input',
+    select: 'select',
+    button: 'btn',
+    main: 'main',
+    archive: 'archive',
+    header: 'header',
+    footer: 'footer',
+    progress: 'progress',
+  }
 })])
   .process(html)
   .then((result) => {
